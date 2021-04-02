2f2e05e816caaa68ce93086350b4c7c8b44a00d1
“julianallenderussek” <jallende@bu.edu>
2021-04-02 16:14:38 -0600

    commit

--- script.js
+++ script.js
@@ -77,3 +77,87 @@ function validation() {
     alert('Gracias por contactarnos. Un correo electronico fue enviado a tu mail')
     return true
 }
+
+
+//Toggle Info Menu
+toggleCount = 0
+
+innerHTML1 = '<h3 class="title-paquete">Paquete 1</h3> <ul class="list-paquete"> <li>Este es el paquete 1</li><li>ESTE ES EL PAQUETE 1</li><li>ESTE ES EL PAQUETE Uno</li></ul>'
+innerHTML2 = '<h3 class="title-paquete">Paquete 2</h3> <ul class="list-paquete"> <li>Este es el paquete 2</li><li>ESTE ES EL PAQUETE 2</li><li>ESTE ES EL PAQUETE DOS</li></ul>'
+innerHTML3 = '<h3 class="title-paquete">Paquete tres</h3> <ul class="list-paquete"> <li>Este es el paquete 4</li><li>ESTE ES EL PAQUETE 3</li><li>ESTE ES EL PAQUETE tres</li></ul>'
+innerHTML4 = '<h3 class="title-paquete">Paquete 4</h3> <ul class="list-paquete"> <li>Este es el paquete 4</li><li>ESTE ES EL PAQUETE 2</li><li>ESTE ES EL PAQUETE CUATRO</li></ul>'
+
+
+displayContainer = document.getElementsByClassName("display_container")
+button1 = document.getElementsByClassName('btn1')
+button2 = document.getElementsByClassName('btn2')
+button3 = document.getElementsByClassName('btn3')
+button4 = document.getElementsByClassName('btn4')
+
+
+
+function dropdown1() {
+  if (toggleCount == 0) {
+    displayContainer[0].innerHTML = innerHTML1
+    displayContainer[0].style.display = 'flex'
+    button1[0].src = '/assets/Avio_WEB_Asset-18.png'
+    toggleCount = 1;
+  } else if (toggleCount == 1) {
+    displayContainer[0].style.display = 'none'
+    toggleCount = 0;
+    button1[0].src = '/assets/155x50.png'
+    button2[0].src = '/assets/155x50.png'
+    button3[0].src = '/assets/155x50.png'
+    button4[0].src = '/assets/155x50.png'
+  }
+
+}
+
+function dropdown2() {
+  if (toggleCount == 0) {
+    displayContainer[0].innerHTML = innerHTML2
+    displayContainer[0].style.display = 'flex'
+    button2[0].src = '/assets/Avio_WEB_Asset-18.png'
+    toggleCount = 1;
+  } else if (toggleCount == 1) {
+    displayContainer[0].style.display = 'none'
+    toggleCount = 0;
+    button1[0].src = '/assets/155x50.png'
+    button2[0].src = '/assets/155x50.png'
+    button3[0].src = '/assets/155x50.png'
+    button4[0].src = '/assets/155x50.png'
+  }
+}
+
+function dropdown3() {
+  if (toggleCount == 0) {
+    displayContainer[0].innerHTML = innerHTML3
+    displayContainer[0].style.display = 'flex'
+    button3[0].src = '/assets/Avio_WEB_Asset-18.png'
+    toggleCount = 1;
+  } else if (toggleCount == 1) {
+    displayContainer[0].style.display = 'none'
+    toggleCount = 0;
+    button1[0].src = '/assets/155x50.png'
+    button2[0].src = '/assets/155x50.png'
+    button3[0].src = '/assets/155x50.png'
+    button4[0].src = '/assets/155x50.png'
+  }
+
+}
+function dropdown4() {
+  if (toggleCount == 0) {
+    displayContainer[0].innerHTML = innerHTML4
+    displayContainer[0].style.display = 'flex'
+    button4[0].src = '/assets/Avio_WEB_Asset-18.png'
+    toggleCount = 1;
+  } else if (toggleCount == 1) {
+    displayContainer[0].style.display = 'none'
+    toggleCount = 0;
+    button1[0].src = '/assets/155x50.png'
+    button2[0].src = '/assets/155x50.png'
+    button3[0].src = '/assets/155x50.png'
+    button4[0].src = '/assets/155x50.png'
+  }
+
+}
